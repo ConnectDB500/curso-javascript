d952928f7e8881b9ea88f80f88b44f4a229a1cfb
Connect <liberalinoenterprise500@gmail.com>
2025-08-04 00:01:07 -0300

    Add algorithms

--- react/meu-projeto/src/components/Form.js
+++ react/meu-projeto/src/components/Form.js
@@ -1,15 +1,38 @@
+import {useState} from 'react'
+
 function Form() {
   function cadastrarUsuario(e){
     e.preventDefault()
     console.log("Usuario cadastrado!")
+    console.log(`Usuario ${name} foi cadastrado com a senha: ${password}`)
   }
+
+  const [name, setName] = useState()
+  const [password, setPassword] = useState()
 
   return (
     <div>
       <h1>Meu cadastro: </h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="digite o seu nome" />
+          <label htnlFor="name">Nome: </label>
+          <input 
+          type="text" 
+          id="name" 
+          name="name"
+          placeholder="Digite o seu nome" 
+          onChange={(e) => setName(e.target.value)}
+        />
+        </div>
+        <div>
+          <label htnlFor="password">Senha: </label>
+          <input 
+          type="password" 
+          id="password"
+          name="password"
+          placeholder="digite o seu nome" 
+          onChange={(e) => setPassword(e.target.password)}
+        />
         </div>
         <div>
           <input type="submit" value="Cadastro" />
